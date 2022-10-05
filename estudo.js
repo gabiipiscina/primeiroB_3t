0d495182f392a9a0d8189fe53824c90f45a832bc
gabiipiscina <110116757+gabiipiscina@users.noreply.github.com>
2022-10-05 12:28:55 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -17,6 +17,15 @@ document.write("  Tabuada do " + tabuada + " <br>");
  document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>") ;
  document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>") ;
  document.write(tabuada + " x 10 = " + (tabuada*10)+"<br>") ;
+}
+let lista= ["Jesus","Spessato","Duarte","Jorge","Neuza","Gilmar","Jossyara"]; 
 
+function MostraLista (){
+document.write("Tamanho da lista: " + lista.length + "<br>");
+document.write ("Professor: " + lista [1] + "<br>");
+for(let i = 0; 1 < lista.length; i++) {
+    document.write ("Professor: " + lista [i] + "<br>");
 
 }
+}
+
