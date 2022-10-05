bcc25d25a35d63400f603be8aa0d5a4f422f721e
gabiipiscina <110116757+gabiipiscina@users.noreply.github.com>
2022-10-05 12:46:48 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,30 +2,39 @@ let tabuada = 6;
 
 
 
-function escreva (){
-document.write("  Tabuada do " + tabuada + " <br>");
+function escreva() {
+    document.write("  Tabuada do " + tabuada + " <br>");
 
 
 
- document.write(tabuada + " x 1 = " + (tabuada*1) +"<br>") ;
- document.write(tabuada + " x 2 = " + (tabuada*2)+"<br>") ;
- document.write(tabuada + " x 3 = " + (tabuada*3)+"<br>") ;
- document.write(tabuada + " x 4 = " + (tabuada*4)+"<br>") ;
- document.write(tabuada + " x 5 = " + (tabuada*5)+"<br>") ;
- document.write(tabuada + " x 6 = " + (tabuada*6)+"<br>") ;
- document.write(tabuada + " x 7 = " + (tabuada*7)+"<br>") ;
- document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>") ;
- document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>") ;
- document.write(tabuada + " x 10 = " + (tabuada*10)+"<br>") ;
+    document.write(tabuada + " x 1 = " + (tabuada * 1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada * 2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada * 3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada * 4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada * 5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada * 6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada * 7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada * 8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada * 9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada * 10) + "<br>");
 }
-let lista= ["Jesus","Spessato","Duarte","Jorge","Neuza","Gilmar","Jossyara"]; 
+let lista = ["Jesus", "Spessato", "Duarte", "Jorge", "Neuza", "Gilmar", "Jossyara"];
 
-function MostraLista (){
-document.write("Tamanho da lista: " + lista.length + "<br>");
-document.write ("Professor: " + lista [1] + "<br>");
-for(let i = 0; 1 < lista.length; i++) {
-    document.write ("Professor: " + lista [i] + "<br>");
-
+function mostraLista() {
+    document.write("Tamanho da lista: " + lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write("Professor: " + lista[i]);
+    }
 }
+function multiplica() {
+    for (let i = 1; i <= 10; i++) {
+        document.write("0 valor do i= " + i + "<br>");
+        for (let j = 1; j <= 10; j++) {
+            document.write(i + " x " + j + " = " + (i * j) + "<br>")
+
+        }
+
+    }
+
 }
 
