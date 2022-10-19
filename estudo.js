c52c94b32cf50980fd35b2b96c8563f1af7bb68e
gabiipiscina <110116757+gabiipiscina@users.noreply.github.com>
2022-10-19 12:45:52 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -45,3 +45,39 @@ function total(){
     document.write("Resultado: " + r);
 
 }
+function media(){
+let n1 = document.getElementById("n1").value;
+let n2 = document.getElementById("n2").value;
+let n3 = document.getElementById("n3").value;
+let n4 = document.getElementById("n4").value;
+
+let r = (Number(n1)+Number(n2)+Number(n3)+Number(n4))/4;
+document.getElementById("result").innerHTML = "Média:" + r;
+}
+function soma(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("n4").value;
+    
+    let r = (Number(n1)+Number(n2)+Number(n3)+Number(n4));
+    document.getElementById("result").innerHTML = "Somar:" + r;
+    }
+    function Multiplicar(){
+        let n1 = document.getElementById("n1").value;
+        let n2 = document.getElementById("n2").value;
+        let n3 = document.getElementById("n3").value;
+        let n4 = document.getElementById("n4").value;
+        
+        let r = (Number(n1)*Number(n2)*Number(n3)*Number(n4));
+        document.getElementById("result").innerHTML = "Multiplicar:" + r;
+        }
+        function Dividir(){
+            let n1 = document.getElementById("n1").value;
+            let n2 = document.getElementById("n2").value;
+            let n3 = document.getElementById("n3").value;
+            let n4 = document.getElementById("n4").value;
+            
+            let r = (Number(n1)/Number(n2)/Number(n3)/Number(n4));
+            document.getElementById("result").innerHTML = "Dividir:" + r;
+            }
